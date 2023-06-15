10cba38843354aae197b8e82b2b48538c7ed7a5e
jochevipe <127540988+jochevipe@users.noreply.github.com>
2023-06-15 17:26:38 +0000

    Extra en struct carta y arreglado el crearCarta (aleatoriedad), terminado el mostrarMano para uso actual
    Las cartas ya se guardan correctamente junto con los perfiles

---
+++ list.h
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+#ifndef _list_h
+#define _list_h
+
+typedef struct List List;
+typedef List Stack;
+typedef struct Node Node;
+/* list operations */
+Node * createNode(void * data);
+List * createList();
+bool is_empty(List* list);
+void * firstList(List * list);
+void * nextList(List * list);
+void * lastList(List * list);
+void * prevList(List * list);
+void pushFront(List * list, void * data);
+void pushBack(List * list, void * data);
+void pushCurrent(List * list, void * data);
+void pushFrontCircular(List * list, void * data);
+void *popFront(List *list);
+void * popBack(List * list);
+void *popCurrent(List * list);
+List *copyList(List *src);
+void cleanList(List * list);
+
+/* stack operations */
+Stack* createStack();
+void pop(Stack* s);
+void* top(Stack* s);
+void push(Stack* s, void* data);
+
+#endif /* _list_h */
--- main.c
+++ main.c
@@ -1,7 +1,195 @@
-
+#include "list.h"
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#define MAXCHAR 20
+#define barrita "\n======================================\n"
+#define barrita2 "\n--------------------------------------\n"
+
+typedef struct {
+  int numero;
+  bool especial;
+  char color[9];
+  char tipo[15];
+} tipoCarta;
+
+typedef struct {
+  List *mano;
+  char nombre[20];
+  int numCartas;
+} tipoJugador;
+
+typedef struct {
+  tipoCarta *ultimaJugada;
+  bool reversa;
+} tipoPartida;
+
+tipoCarta *crearCarta() {
+
+  tipoCarta *carta= (tipoCarta*) malloc(sizeof(tipoCarta));
+  carta->especial = false;
+  carta->numero = (rand() % 14) + 1;
+
+  /*
+  Cancelar turno : 10
+  Invertir turno : 11
+  +2 : 12
+  +4 : 13
+  Cambiar color : 14
+  */
+  if(carta->numero < 13){
+    int colorNumero = (rand() % 4) + 1;
+    
+    switch (colorNumero){
+    case 1:
+      strcpy(carta->color, "Rojo");
+      break;
+    case 2:
+      strcpy(carta->color, "Verde");
+      break;
+    case 3:
+      strcpy(carta->color, "Azul");
+      break;
+    case 4:
+      strcpy(carta->color, "Amarillo");
+      break;
+    }
+  }
+  
+  if(carta->numero > 9){
+    int numeroEspecial = (rand() % 5) + 10;
+    carta->especial = true;
+    
+    switch (numeroEspecial){
+    case 10:
+      strcpy(carta->tipo, "Cancelar");
+      break;
+    case 11:
+      strcpy(carta->tipo, "Invertir");
+      break;
+    case 12:
+      strcpy(carta->tipo, "+2");
+      break;
+    case 13:
+      strcpy(carta->color, "+4");
+      break;
+    case 14:
+      strcpy(carta->tipo, "Cambia color");
+      break;
+    }
+  }
+
+  return carta;
+}
+
+bool esValida(tipoCarta carta, tipoCarta ultimaJugada) {
+  // sin tienen igual simbolo se puede tirar, reserva, +2, +4, saltar estan
+  // incluidos
+  if (carta.numero == ultimaJugada.numero)
+    return true;
+
+  // sin tienen igual color se puede tirar, reserva, +2, +4, saltar estan
+  // incluidos
+  if (strcmp(carta.color, ultimaJugada.color))
+    return true;
+
+  return false;
+}
+
+void iniciarPartiada(tipoPartida* juego, List *listaJuego) {
+  
+  for (int i = 0; i < 4; i++) {
+    
+    tipoJugador* jugadorAux = (tipoJugador*) malloc(sizeof(tipoJugador));
+    
+    jugadorAux->numCartas = 7;
+    
+    if (i == 0){
+      char nombreAux[20];
+      printf("indica tu nombre\n");
+      scanf("%s", nombreAux);
+      while (getchar() != '\n');
+      
+      strcpy(jugadorAux->nombre, nombreAux);
+    }else if(i == 1){
+      strcpy(jugadorAux->nombre, "CLAUDIO TOLEDO");
+    }else if(i == 2){
+      strcpy(jugadorAux->nombre, "MOHAMED DIAS");
+    }else{
+      strcpy(jugadorAux->nombre, "MR.HUNDOR");
+    }
+    
+    jugadorAux->mano = createList();
+
+    
+    for (int f = 0; f < 7; f++) {
+      tipoCarta *carta = crearCarta();
+      pushBack(jugadorAux->mano, carta);
+    }
+    
+    pushFrontCircular(listaJuego, jugadorAux);
+  }
+}
+
+void mostrarMano(tipoJugador *jugadorActual){
+
+  tipoCarta *carta=firstList(jugadorActual->mano);
+  printf("Jugador: %s\n",jugadorActual->nombre);
+  puts("Mazo:\n");
+  
+  carta = firstList(jugadorActual->mano);
+  while(carta!=NULL){
+
+    printf("%s\n",carta->tipo);
+    if(carta->numero < 12)
+      printf("%s\n",carta->color);
+    if(!carta->especial)
+      printf("%d\n",carta->numero);
+    
+    puts("");
+    
+    carta=nextList(jugadorActual->mano);
+  }
+    
+
+
+}
+
+void jugarCarta(tipoPartida *juego,tipoJugador *jugadorActual){
+
+  mostrarMano(jugadorActual);
+
+  while(1){
+    
+  }
+}
+
 int main(void) {
-  printf("Hello World\n");
-  printf("prueba");
+
+  srand(time(0)); //Funcion para asignar numeros aleatorios a las cartas 
+  
+  tipoPartida *juego = (tipoPartida*) malloc(sizeof(tipoPartida));
+  juego->ultimaJugada = crearCarta();
+
+  List *listaJuego = createList();
+  iniciarPartiada(juego, listaJuego);
+
+  tipoJugador* jugadorActual = (tipoJugador*) malloc(sizeof(tipoJugador));
+  jugadorActual=firstList(listaJuego);
+
+  /*
+    Deberiamos hacer un ciclo infinito de jugar carta que englobe las demás
+    funciones hasta llegar a la condicion de que alguien se haya quedado sin
+    cartas y ser el ganador
+
+    No sé si prefieren hacer el loop en el main o dentro de la f(x) jugarCarta
+  */
+  jugarCarta(juego,jugadorActual);
+  
+ 
+
+  
   return 0;
 }
